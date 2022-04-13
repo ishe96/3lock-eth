01a523e995ed401b7ef115db1d34e6d192c9d5a9
Isheanesu Madamombe <amadamombe904@gmail.com>
2022-04-13 23:36:53 +0200

    heroku crypto init

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => {
 
             <div className="flex justify-center items-center flex-col mt-5">
                 <p className='text-white text-sm text-center'>Join the family</p>
-                <p className='text-white text-sm text-center cursor-pointer'>info@3ankcrypto.com</p>
+                {/* <p className='text-white text-sm text-center cursor-pointer'>info@3ankcrypto.com</p> */}
             </div>
 
             <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5"/>
